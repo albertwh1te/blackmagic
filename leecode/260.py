53a9a827cbc12c63b42df39fc74ab2063ede6f47
mark <iamwh1temark@outlook.com>
2016-05-16 11:08:22 +0800

    Please enter the comm message for your changes. Lines starting

--- leecode/260.py
+++ leecode/260.py
@@ -4,4 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        
+        nums_dict = {}
+        result = []
+        for i in nums:
+            if i in nums_dict:
+                nums_dict[i] = 2
+            else:
+                nums_dict[i] = 1
+    
+        for i in nums_dict:
+            if nums_dict[i] == 1:
+                result.append(i)
+    
+        return result
+    
+t = Solution()
+print t.singleNumber([1, 2, 1, 3, 2, 5])
