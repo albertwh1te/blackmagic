afc3948bd0b2d98ac00659194fbb1072634583d9
mark <iamwh1temark@outlook.com>
2016-05-16 18:29:54 +0800

    finished 326

---
+++ leecode/326.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        import math
+        cc = math.log(n,3)
+        if 3 ** cc == n:
+            return True
+        else:
+            return False
+        
+        
