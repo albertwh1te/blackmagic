1d3309d0bc2b6f883c69138f9b53fd8876e37800
mark <iamwh1temark@outlook.com>
2016-05-17 19:06:41 +0800

    finished 83

---
+++ leecode/83.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return head
+        p = head
+        while p.next:
+            if p.val == p.next.val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return head
