da6e7a8067a2c80463b16341f4bb808b022ecb14
mark <iamwh1temark@outlook.com>
2016-05-18 19:36:55 +0800

    half way

--- leecode/263.py
+++ leecode/263.py
@@ -4,5 +4,28 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
+        if num == 1:
+            return True
+        elif num in [2,3,5]:
+            return True
+        elif num >1 and num not in [2,3,5]:
+            a = ([i, num//i] for i in range(1, int(num**0.5)+1) if num % i == 0)
+            factors = set(reduce(list.__add__,a))
+            print factors
+            print factors.difference([1,num]).difference([2,3,5])
+            if factors.difference([1,num]).difference([2,3,5]) == set([]):
+                return True
+            else:
+                return False
+    
+            
+t = Solution()
+print t.isUgly(2)    
+# print t.isUgly(6)    
+print t.isUgly(7)    
+print t.isUgly(9)    
+
+        
+    
         
         
