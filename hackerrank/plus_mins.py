9fcfc050e9ca8f3817e31bcb186154b77573041f
mark <iamwh1temark@outlook.com>
2016-05-24 18:34:39 +0800

    start hackerrank

---
+++ hackerrank/plus_mins.py
@@ -0,0 +1,31 @@
+# def solution():
+#     pos = neg = zero = 0
+#     for i in arr:
+#         if i > 0:
+#             pos += 1
+#         elif i == 0:
+#             zero += 1
+#         else:
+#             neg += 1
+#     print("%6.6f" % (float(pos)/float(n)))
+#     print("%6.6f" % (float(zero)/float(n)))
+#     print("%6.6f" % (float(neg)/float(n)))
+# n = 4    
+# def solution():
+#     for i in xrange(n):
+#         print ' ' * (n-1) + '#' * i 
+#     return True
+# 
+# solution()
+# time = "07:05:45PM"
+# def solution():
+#     true_time  = time[:-2]
+#     [hh, mm, ss] = true_time.split(':')
+#     if "PM" in time:
+#         hh = str(int(hh) + 12)
+#     print ":".join([hh, mm, ss])
+#     return True
+# solution()
+
+        
+    
