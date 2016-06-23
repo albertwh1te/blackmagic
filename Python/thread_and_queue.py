f09c1484f63300a66a63d4032e2979dc36c2ec35
mark <iamwh1temark@outlook.com>
2016-06-23 11:44:23 +0800

    add download img mixin

---
+++ Python/thread_and_queue.py
@@ -0,0 +1,36 @@
+from Queue import Queue
+from threading import Thread
+import requests
+import time
+
+
+hosts = ["https://www.baidu.com/", "https://stackoverflow.com/#", "https://github.com/"]
+
+queue = Queue()
+
+class RequestWithThread(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+    
+    def run(self):
+        host = self.queue.get()
+        url = requests.get(host)
+        print url.title
+        self.queue.task_done()
+
+start = time.time()
+def main():
+    for i in range(5):
+    
+
+        
+        
+
+        
+        
+        
+                
+
+
+
