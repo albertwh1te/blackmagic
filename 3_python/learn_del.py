7771921df40bebd21fb53729396573c06675da7b
mark <iamwh1temark@outlook.com>
2016-07-07 11:10:39 +0800

    python can do del too

---
+++ 3_python/learn_del.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+
+class test_del():
+    def __init__(self,name):
+        self.name = name
+        print("my name is %s" %self.name)
+    
+    def __del__(self):
+        print("%s has been deleted" %self.name)
+    
+if __name__ == '__main__':
+     dd = test_del("bobo")
+     del dd
+
