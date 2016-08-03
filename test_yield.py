7a2fe4945489ad34f5c6821033d16862fc241c4e
MarkWh1te <iamwh1temark@outlook.com>
2016-08-03 22:59:31 +0800

    push

--- test_yield.py
+++ test_yield.py
@@ -1,10 +1,12 @@
 # coding:utf-8
 
+
 def test_yield(test_list):
     for x in test_list:
         yield x * x
 
 
-cc  = test_yield(range(1,10))
+cc = test_yield(range(1, 10))
+
 for w in cc:
-    print w 
+    print w
