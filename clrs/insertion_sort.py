68c7062b5c25fec0cb7f91d25f5a8554be9a60df
MarkWh1te <iamwh1temark@outlook.com>
2016-08-28 02:20:27 +0800

    insertion sort

---
+++ clrs/insertion_sort.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+test_arr = [33, 4, 55, 999, 10, 88]
+print test_arr
+result_arr = insertion_sort(test_arr)
+print result_arr
