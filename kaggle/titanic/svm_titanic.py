661fb755d798665d229d13144bbee7a2694cf22d
mark <iamwh1temark@outlook.com>
2017-01-17 14:47:58 +0800

    separate train and test data set to ensure we are not overfitting

--- kaggle/titanic/svm_titanic.py
+++ kaggle/titanic/svm_titanic.py
@@ -7,17 +7,17 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    from pre_process import X_train,Y_train,X_test,train_raw,test_raw
+    from pre_process import X_train,Y_train,X_test,Y_test,test_raw,X_pred
     from sklearn import svm
     # kernel rbf means Gaussian
     rbf_svm = svm.SVC(kernel='rbf')
 
     print(X_train[:5],Y_train[:5])
     rbf_svm.fit(X_train,Y_train)
-    score = rbf_svm.score(X_train,Y_train)
-    # print(score)
+    score = rbf_svm.score(X_test,Y_test)
+    print(score)
 
-    rbf_svm_pred = rbf_svm.predict(X_test)
+    rbf_svm_pred = rbf_svm.predict(X_pred)
 
     submission = pd.DataFrame({
         'PassengerId':test_raw['PassengerId'],
