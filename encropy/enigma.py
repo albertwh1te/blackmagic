5ba7bbd794e88c3f73f9c26098e3389d9a4497dd
markwh1te <iamwh1temark@outlook.com>
2017-06-16 03:22:57 -0700

    change back

--- encropy/enigma.py
+++ encropy/enigma.py
@@ -1,27 +1,28 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- # wrappers of symmetric encryption algorithms with python
 import base64
 import hashlib
 from Crypto import Random
-from Crypto.Cipher import Blowfish,AES
+from Crypto.Cipher import Blowfish,AES,DES
 
 class BaseCipher(object):
 
     def __init__(self,key,cipher):
         self.block_size = cipher.block_size
         self.key = hashlib.sha256(key.encode()).digest()
-        # cipher object crypto
+        # cipher object
         self.cipher = cipher
-        # true crypter for use
 
     def encrypt(self, raw):
         raw = self._pad(raw)
         iv = Random.new().read(self.block_size)
+        # true crypter for encrypt
         self.crypter = self.cipher.new(self.key,self.cipher.MODE_CBC,iv)
         return base64.b64encode(iv + self.crypter.encrypt(raw))
 
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
         iv = enc[:self.block_size]
+        # true crypter for decrypt
         self.crypter = self.cipher.new(self.key,self.cipher.MODE_CBC,iv)
         return self._unpad(self.crypter.decrypt(enc[self.block_size:])).decode('utf-8')
 
@@ -39,9 +40,10 @@ def cipher_factory(key,cipher_name):
     if cipher_name == "AES":
         return BaseCipher(key,AES)
 
+
 if __name__ == '__main__':
-    # cipher = cipher_factory('test',"AES")
-    cipher = cipher_factory('test',"Blowfish")
+    cipher = cipher_factory('test',"AES")
+    # cipher = cipher_factory('test',"Blowfish")
     plain_text = "this is a long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long text"
     encrypted = cipher.encrypt(plain_text)
     text = cipher.decrypt(encrypted)
