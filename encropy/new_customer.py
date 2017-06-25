0c73625378f34d9e9f75d0ece5c15fec365cd578
MarkWh1te <iamwh1temark@outlook.com>
2017-06-26 07:45:14 +0800

    udate

--- encropy/new_customer.py
+++ encropy/new_customer.py
@@ -1,13 +1,38 @@
 # -*- coding: utf-8 -*-
-
-
+import binascii
 
 def toBinary(s):
-    return "".join(format(ord(x), 'b') for x in s)
+    print(int(binascii.hexlify(s),16))
+    return bin(int(binascii.hexlify(s),16))
+
+def decodeBinary(s):
+    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
+
+# def toBinary(s):
+#     return "".join(format(ord(x), 'b') for x in s)
+
 
 def encrypt(s,key):
     b = toBinary(s)
+    print(b)
+    print(len(b))
     bitkey = toBinary(key)
     for i in range(len(b)):
-        for j in b:
-            reduce(lambda x:j^x,list(b))
+        b[i] = str(reduce(lambda x,y:int(x)^int(y),list(bitkey),b[i]))
+    return " ".join(b)
+
+
+def decrypt(s,key):
+    b = list(toBinary(s))
+    dee = b
+    bitkey = toBinary(key)
+    for i in range(len(b)):
+        b[i] = str(reduce(lambda x,y:int(x)^int(y),list(bitkey),dee[i]))
+    return " ".join(b)
+
+key = "fffffff"
+s = "hi"
+enc = encrypt(s,key)
+text = decrypt(enc,key)
+print(enc)
+print(text)
