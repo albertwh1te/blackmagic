9d3f950478dfe02806d052f6e2f0e76db73e2683
MarkWh1te <iamwh1temark@outlook.com>
2017-11-25 11:55:24 +0800

    review decorate

--- from_guoguo/python/decorate_example.py
+++ from_guoguo/python/decorate_example.py
@@ -1,1 +1,20 @@
 # -*- coding=utf-8 -*-
+import time
+def time_cost(func):
+    def wrapper(**kwargs):
+        print(kwargs)
+        start = time.clock()
+        func()
+        cost = time.clock() - start
+        print('the cost time is ', cost)
+    return wrapper
+
+@time_cost
+def test_func(time=None):
+    sum = 0
+    for i in range(1000):
+        sum += i
+    print(sum)
+
+if __name__ == "__main__":
+    test_func(time=3)
