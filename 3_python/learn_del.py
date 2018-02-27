e352171d8ef40ecd59b1a06b355b4c003c8aed2c
MarkWh1te <iamwh1temark@outlook.com>
2018-02-27 09:57:47 +0800

    add fun zip test

--- 3_python/learn_del.py
+++ 3_python/learn_del.py
@@ -3,14 +3,14 @@
 
 
 class test_del():
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
-        print("my name is %s" %self.name)
+        print("my name is %s" % self.name)
 
     def __del__(self):
-        print("%s has been deleted" %self.name)
+        print("%s has been deleted" % self.name)
+
 
 if __name__ == '__main__':
-     dd = test_del("bobo")
-     del dd
-
+    dd = test_del("bobo")
+    del dd
