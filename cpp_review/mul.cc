51f2e6a0bf2101949deddbed75663d217efe8cc3
MarkWh1te <iamwh1temark@outlool.com>
2018-06-26 23:10:01 +0800

    add product

---
+++ cpp_review/mul.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    std::cout << "please input two number" << std::endl;
+    int v1 = 0;
+    int v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "the product of " << v1 << " and " << v2 << " is " << v1*v2 << std::endl;
+    return 0;
+}
+
